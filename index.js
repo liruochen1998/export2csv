35b80f3501304809c2205057d9bb15817db8a673
Ruochen Li <liruochenv@gmail.com>
2020-11-04 17:33:25 -0500

    initial change after some tests

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function export2csv(input) {
+export function export2csv(input) {
     let csvContent = "data:text/csv;charset=utf-8," + input.map(e => e.join(",")).join("\n");
     var encodeedUri = encodeURI(csvContent);
     window.open(encodeedUri);
